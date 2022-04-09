7c2e3fb71a0f595f65904a24b5c0b50213454ed9
filmangullo <filmantapgullo@gmail.com>
2022-04-09 19:31:28 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <LogoComponent />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LogoComponent from './components/LogoComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LogoComponent
   }
 }
 </script>
---
+++ src/components/LogoComponent.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="wrapper">
+        <h2>Find Best Logo</h2>
+        <div>
+            <div class="content">
+                <img src="" alt="" />
+            </div>
+            <div class="content">
+                <img src="" alt="" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'LogoComponent'
+    }
+</script>
+
+<style scoped>
+    .logo {
+        width: 70%;
+        overflow: hidden;
+        margin: auto;
+    }
+
+
+    .content {
+        width: 150px;
+        height: 150px;
+        background-color: cyan;
+        float: left;
+        margin-left: 2px;
+        margin-right: 2px;
+        margin-top: 4px;
+    }
+</style>
